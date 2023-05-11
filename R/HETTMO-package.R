61d62472dd15b8cb01655cdbfa3ed223c2a18fcf
JudithBouman2412 <judithabouman@gmail.com>
2023-05-11 09:41:22 +0200

    Initial upload Stan code

---
+++ R/HETTMO-package.R
@@ -0,0 +1,16 @@
+#' The 'HETTMO' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name HETTMO-package
+#' @aliases HETTMO
+#' @useDynLib HETTMO, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2022). RStan: the R interface to Stan. R package version 2.21.7. https://mc-stan.org
+#'
+NULL
